84e6896237c3db10bab09c71d8bf1f2d044d9c9c
hyukjin0419 <21900771@handong.ac.kr>
2023-05-11 15:57:31 +0900

    주문 구조체 생성

--- order.h
+++ order.h
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <string>
 //아무래도 방은 구조체로 생성해야 할 것 같음.
+
+using namespace std;
+
+struct Order{
+    string name;
+    string menu;
+    int price;
+    string phoneNum;
+    string accout;
+    Order* next;
+};
+
 class GroupDelivery{
     private:
         
