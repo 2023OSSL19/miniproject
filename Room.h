a9e439d49fcbd1d3b58f08f58ea3f757d5a959aa
Snout813 <21900467@handong.ac.kr>
2023-05-13 21:19:28 +0900

    기타 정보 입력

--- Room.h
+++ Room.h
@@ -1,15 +1,26 @@
 # define MAX 10 // 최대 주문 개수는 10개로 제한
+
 typedef struct {
-    char name[50];
+    char name[50]; // 
     int price;
     char memo[150];
+    
 
 }menu; // 메뉴 출력을 위한 struct
+
+typedef struct{
+    int hour;
+    int min;
+}time; // 시간 출력용 struct
+
 class Room{
     private :
         char storeName[50]; // 가게 이름
         int account; // 계좌 번호
         int totalPrice; // 총 가격
+        char place[100]; // 도착 장소 
+        time t1; // 마감 시간 출력용 변수
+
 
 
 
@@ -19,7 +30,9 @@ class Room{
         Room(); // constructor
         ~Room(); // destructor
         void SearchRoom(); // 메뉴를 입력하면 방을 해당 방을 출력
-        void createList(); // 
-        
+        void CreateRoom(); // 방 생성
+        void DeleteRoom(); // 방 삭제
+        void UpdateRoom(); // 방 정보 업데이트
+        void ShowMenu(); // 메뉴 출력 기능
         
 }
