f794fe86b6b4a94d6ae35c34b303397ec10f2ae4
유진우 <sygyzy@yujin-uui-MacBookPro.local>
2023-05-13 21:13:05 +0900

    Create CRUD

--- Room.h
+++ Room.h
@@ -1,12 +1,25 @@
+# define MAX 10 // 최대 주문 개수는 10개로 제한
+typedef struct {
+    char name[50];
+    int price;
+    char memo[150];
+
+}menu; // 메뉴 출력을 위한 struct
 class Room{
     private :
         char storeName[50]; // 가게 이름
-        
+        int account; // 계좌 번호
+        int totalPrice; // 총 가격
+
+
+
+
 
     public: 
         Room(); // constructor
         ~Room(); // destructor
         void SearchRoom(); // 메뉴를 입력하면 방을 해당 방을 출력
-        void createList();
+        void createList(); // 
+        
         
 }
