fb6bc247f2e8d6a237c26fda9be203939722dcef
유진우 <sygyzy@yujin-uui-MacBookPro.local>
2023-05-13 20:58:32 +0900

    search room, add list

--- Room.h
+++ Room.h
@@ -1,8 +1,12 @@
 class Room{
     private :
+        char storeName[50]; // 가게 이름
         
+
     public: 
-        Room();
-        ~Room();
+        Room(); // constructor
+        ~Room(); // destructor
+        void SearchRoom(); // 메뉴를 입력하면 방을 해당 방을 출력
+        void createList();
         
 }
