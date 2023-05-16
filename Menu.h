9f78e7ee32ca1f7837c860dbb75ad9d9e9886c47
hyukjin0419 <21900771@handong.ac.kr>
2023-05-16 15:15:08 +0900

    fixing capitals

--- Menu.h
+++ Menu.h
@@ -2,10 +2,9 @@ typedef struct {
     char name[50]; // 메뉴 이름
     int price; // 가격
     char memo[150]; // 기타 사항
-    
-
 }menu; // 메뉴 출력을 위한 struct
-void CreateMenu(Menu *m[], int *num);
-void ShowMenu(Menu *m[], int num);
-void DeleteMenu(Menu *m[], int *num);
-void UpdateMenu(Menu *m[], int num);
+
+void createMenu(Menu *m[], int *num);
+void showMenu(Menu *m[], int num);
+void deleteMenu(Menu *m[], int *num);
+void updateMenu(Menu *m[], int num);
