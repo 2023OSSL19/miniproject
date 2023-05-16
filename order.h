d093d8527f08e893157dfe486f4e363d6911de58
hyukjin0419 <21900771@handong.ac.kr>
2023-05-16 17:16:17 +0900

    05.16 discussion results

--- order.h
+++ order.h
@@ -10,6 +10,7 @@ typedef struct {
     int price;
     string phoneNum;
     string accout;
+    bool paid = false; //결제 여부 
     Order* next;
 }order;
 
@@ -17,17 +18,17 @@ typedef struct {
 class Order{
     private :
         int count; // 총 주문 갯수
-        room* head =NULL; // linked list로 구현 head
+        room* head = nullptr; // linked list로 구현 head
 
     public: 
-        Room(); // constructor, 최대 방 개수 100개로 제한, 메뉴 개수 15개로 제한
-        ~Room(); // destructor
-        void searchRoom(); // 메뉴를 입력하면 방을 해당 방을 출력, 사용자에서 사용
-        void createRoom(); // 방 생성, 방장에서만 사용
-        void deleteRoom(); // 방 삭제, 방장에서만 사용
-        void updateRoom(); // 방 정보 업데이트, 방장에서만 사용
-        void print(); // 전체 방 정보 출력, 사용자에서 사용
-        void showMenu(); // 메뉴 출력 기능
-        bool isEmpty(); // 방 유무 확인
-        void updateMenu(); //메뉴 정보 업데이트
+        Order(); // constructor, 최대 방 개수 100개로 제한, 메뉴 개수 15개로 제한
+        ~Order(); // destructor
+        void searchOrder(); 
+        void createOrder();  
+        void deleteOrder();  
+        void updateOrder();  
+        void print(); 
+        void showOrder();  
+        bool isEmpty();  
+        void updateOrder(); 
 }
