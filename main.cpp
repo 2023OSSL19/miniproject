04c6cbb3cdfd635291706cefa2ec145c79c21394
hyukjin0419 <21900771@handong.ac.kr>
2023-05-18 15:29:11 +0900

    createRoom 기본 구현 기능 생성

--- main.cpp
+++ main.cpp
@@ -4,23 +4,27 @@
 
 using namespace std;
 
-Room functions(){
+Room* functions(){
+    string m;
     cout << "A: 방 만들기" << endl;
     cout << "B: 방 참여하기" << endl;
     cout << "C: 방 목록보기" << endl;
     cout << "원하시는 기능을 선택하세요: ";
     cin >> m;
-
-    if (m == "A"){
-        Room* order = new Room();
-    }
+    
+    Room* order = new Room();
+    
 
     return order;
 }
 
 int main(){
     cout << "어플에 오신걸 환영합니다" << endl;
-    functions();
+    Room* a = functions();
+    a->createRoom();
+    a->createRoom();
+    a->printRoom();
+
 
     return 0;
 }
