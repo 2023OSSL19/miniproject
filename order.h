58f67b17bc0ffa82bc526bd1e401c833689daa2e
hyukjin0419 <21900771@handong.ac.kr>
2023-05-19 14:29:49 +0900

    createOrder 기본 기능 구현

--- order.h
+++ order.h
@@ -1,33 +1,33 @@
 #include <iostream>
 #include <string>
-//아무래도 방은 구조체로 생성해야 할 것 같음.
 
 using namespace std;
 
-typedef struct {
-    string name;
+struct orderNode{
+    string storeName;
     string menu;
     int price;
     string phoneNum;
     string accout;
-    bool paid = false; //결제 여부 
-    Order* next;
-}order;
+    //bool paid = false; //결제 여부 
+    orderNode* next;
+};
 
 
 class Order{
     private :
         int count; // 총 주문 갯수
-        room* head = nullptr; // linked list로 구현 head
+        orderNode* head; // linked list로 구현 head
 
     public: 
         Order(); // constructor, 최대 방 개수 100개로 제한, 메뉴 개수 15개로 제한
         ~Order(); // destructor
         //void searchOrder(); 
-        void createOrder();  
-        void deleteOrder();  
+        void createOrder();
+        //void deleteOrder();
         //void updateOrder();  
-        void print(); 
+        //void printMenu();
+        //void printOrder(); 
         //void showOrder();  
         //bool isEmpty();   
-}
+};
